f5db1755b9aced75e8a7e7ec37ad8cb3c11e9f37
Romain Aroldi <romain.aroldi57@gmail.com>
2023-03-14 14:53:54 +0100

    Add DOM exercise 1

---
+++ DOM/exercice-1/exercice1.js
@@ -0,0 +1,6 @@
+const footerContainer = document.querySelector("div#footer");
+
+const footer = document.createElement("footer");
+footerContainer.appendChild(footer);
+footer.textContent = "Romain AROLDI | tous droits réservés ©";
+footer.classList.add("text-light");
