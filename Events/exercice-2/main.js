5623cec392193396c2a54c8bef3678eb073ac2ff
Romain Aroldi <romain.aroldi57@gmail.com>
2023-03-14 15:55:35 +0100

    Reformat code

--- Events/exercice-2/main.js
+++ Events/exercice-2/main.js
@@ -5,23 +5,23 @@ const dateNaissance = document.querySelector('#date-naissance');
 const message = document.querySelector('#message');
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  // Récupération des valeurs des champs
-  const nomValue = nom.value;
-  const prenomValue = prenom.value;
-  const dateNaissanceValue = dateNaissance.value;
+    // Récupération des valeurs des champs
+    const nomValue = nom.value;
+    const prenomValue = prenom.value;
+    const dateNaissanceValue = dateNaissance.value;
 
-  // Calcul de l'âge en utilisant l'année de naissance
-  const dateNaissanceObj = new Date(dateNaissanceValue);
-  const maintenant = new Date();
-  const age = maintenant.getFullYear() - dateNaissanceObj.getFullYear();
+    // Calcul de l'âge en utilisant l'année de naissance
+    const dateNaissanceObj = new Date(dateNaissanceValue);
+    const maintenant = new Date();
+    const age = maintenant.getFullYear() - dateNaissanceObj.getFullYear();
 
-  // Affichage du message de bienvenue avec l'âge
-  message.textContent = `Bienvenue ${prenomValue} ${nomValue} ! Vous avez ${age} ans.`;
+    // Affichage du message de bienvenue avec l'âge
+    message.textContent = `Bienvenue ${prenomValue} ${nomValue} ! Vous avez ${age} ans.`;
 
-  // Effacement du contenu des champs
-  nom.value = '';
-  prenom.value = '';
-  dateNaissance.value = '';
+    // Effacement du contenu des champs
+    nom.value = '';
+    prenom.value = '';
+    dateNaissance.value = '';
 });
--- Events/exercice-4/main.js
+++ Events/exercice-4/main.js
@@ -6,32 +6,32 @@ const greenInput = document.querySelector('#green');
 const blueInput = document.querySelector('#blue');
 
 randomColorButton.addEventListener('click', () => {
-  const red = Math.floor(Math.random() * 256);
-  const green = Math.floor(Math.random() * 256);
-  const blue = Math.floor(Math.random() * 256);
-  container.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    container.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 });
 
 customColorButton.addEventListener('click', (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const redValue = redInput.value;
-  const greenValue = greenInput.value;
-  const blueValue = blueInput.value;
+    const redValue = redInput.value;
+    const greenValue = greenInput.value;
+    const blueValue = blueInput.value;
 
-  if (!redValue || !greenValue || !blueValue) {
-    alert('Veuillez remplir tous les champs.');
-    return;
-  }
+    if (!redValue || !greenValue || !blueValue) {
+        alert('Veuillez remplir tous les champs.');
+        return;
+    }
 
-  if (redValue < 0 || redValue > 255 || greenValue < 0 || greenValue > 255 || blueValue < 0 || blueValue > 255) {
-    alert('Les valeurs doivent être comprises entre 0 et 255.');
-    return;
-  }
+    if (redValue < 0 || redValue > 255 || greenValue < 0 || greenValue > 255 || blueValue < 0 || blueValue > 255) {
+        alert('Les valeurs doivent être comprises entre 0 et 255.');
+        return;
+    }
 
-  container.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+    container.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
 
-  redInput.value = '';
-  greenInput.value = '';
-  blueInput.value = '';
+    redInput.value = '';
+    greenInput.value = '';
+    blueInput.value = '';
 });
