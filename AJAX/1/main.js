77a74d11cd2a1a2a6f35c44ad1b5b8410db28095
Romain Aroldi <romain.aroldi57@gmail.com>
2023-03-16 09:51:18 +0100

    Add AJAX exercise 1

---
+++ AJAX/1/main.js
@@ -0,0 +1,11 @@
+const container = document.querySelector("div#container");
+
+setTimeout(() => {
+    fetch("citations.txt", {method: "GET"}).then((res) => {
+        if (res.status === 200) {
+            res.text().then((data) => {
+                container.innerHTML = "<p>" + data.replaceAll("\n", "<br>") + "</p>";
+            });
+        }
+    });
+}, 1);
